d3bcc46e0bcb11434b367dbc3adcb53f7f1decb9
Inbar Vernia <inbar.vernia@gmail.com>
2022-04-08 15:52:53 +0100

    Added question view for price ranges

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,9 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-// Run this code when a form is submitted to 'juggling-balls-answer'
+// Run this code when a form is submitted to 'which-ads'
 router.post('/which-ads-answer', function (req, res) {
 
-  // Make a variable and give it the value from 'how-many-balls'
   let whichAds = req.session.data['which-ads']
   let adType = req.session.data['which-advert-type']
 
@@ -14,6 +13,23 @@ router.post('/which-ads-answer', function (req, res) {
   if (whichAds === "Filter" && adType === 'rental') {
     // Send user to next page
     res.redirect('/which-rental-filters')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/not-available')
+  }
+
+})
+
+
+// Run this code when a form is submitted to 'which-rental-filters'
+router.post('/which-rental-filters-answer', function (req, res) {
+
+  let whichRentalFilter = req.session.data['which-rental-filters']
+
+  // Check whether the variable matches a condition
+  if (whichRentalFilter === "price") {
+    // Send user to next page
+    res.redirect('/what-price-range')
   } else {
     // Send user to ineligible page
     res.redirect('/not-available')
